1f59fd5e32417db5a6f1ed143a9327a516868421
Isabella Isaac Fernandes <isabella.fernandes@mrv.com.br>
2020-01-30 10:55:46 -0300

    Projetos Integrados de Aplicações: autenticação do usuário (bcrypt).

--- model/usuario.js
+++ model/usuario.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const usuarioSchema = new Schema({
     nome: { type: String, required: true },
@@ -8,6 +9,16 @@ const usuarioSchema = new Schema({
     moedaTotal: { type: Number, default: 0 },
     moedaTotalMes: { type: Number, default: 10 },
     dataCriacao: { type: Date, default: Date.now }
+});
+
+usuarioSchema.pre('save', function(next){
+    let usuario = this;
+    if(!usuario.isModified('senha')) return next();
+
+    bcrypt.hash(usuario.senha, 10, (err, encrypted) => {
+        usuario.senha = encrypted;
+        return next();
+    });
 });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Usuarios = require('../model/usuario');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     Usuarios.find({}, (err, data) => {
@@ -19,6 +20,23 @@ router.post('/', (req, res) => {
 
         Usuarios.create(req.body, (err, data) => {
             if(err) return res.send({ error: 'Erro ao criar usuários.' });
+            data.senha = undefined;
+            return res.send(data);
+        });
+    });
+});
+
+router.post('/auth', (req, res) => {
+    const { email, senha } = req.body;
+    if(!email || !senha) return res.send({error: 'Dados insuficientes' });
+
+    Usuarios.findOne({email}, (err, data) => {
+        if(err) return res.send({ error: 'Erro ao consultar usuários.' });
+        if(!data) return res.send({ error: 'Usuários não registrado.' });
+
+        bcrypt.compare(senha, data.senha, (err, same) => {
+            if(!same) return res.send({ error: 'Erro ao autenticar usuários.' });
+            data.senha = undefined;
             return res.send(data);
         });
     });

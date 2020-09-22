272612bdb76c1effb9a0dbb7a036ce41aef91ad8
Isabella Isaac Fernandes <isabella.fernandes@mrv.com.br>
2020-09-22 02:40:01 -0300

    Header token

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connection.on('disconnected', () => {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
     next();
 });
 app.use(bodyParser.urlencoded({extended: false}));
